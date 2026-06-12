3d1e1989c4b31dccd0987db23e795ad1b1733b79
Wortexyz <abhijithabhinav144@gmail.com>
2026-06-12 15:34:50 +0500

    Update playermovement , laser

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float movespeed =30f;
+    [Header("Movement Settings")]
+    [SerializeField] float movespeed = 30f;
+    [SerializeField] float  xRotation =20f ;
+     [SerializeField] float zRotation = 40f;
+    [SerializeField]   float RotationSpeed =30f;
     Vector2 movementValue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,18 +20,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         ProcessMovement();
+        ProcessRotation();
 
     }
-
-    private void ProcessMovement()
+ public void OnMove(InputValue value)
+    {
+         movementValue = value.Get<Vector2>();
+    }
+     void ProcessMovement()
     {
         float zoffset = movespeed * -movementValue.x * Time.deltaTime;
         float yoffset = movespeed * movementValue.y * Time.deltaTime;
         transform.localPosition = new Vector3(0, Mathf.Clamp( transform.localPosition.y + yoffset , -3.25f,14), Mathf.Clamp(transform.localPosition.z + zoffset, -15f, 16));
     }
 
-    public void OnMove(InputValue value)
+    void ProcessRotation()
     {
-         movementValue = value.Get<Vector2>();
+        Quaternion targetRotationSide = Quaternion.Euler ( xRotation * -movementValue.x, 0, zRotation * movementValue.y);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotationSide, RotationSpeed *Time.deltaTime);
+
     }
+
+   
 }
